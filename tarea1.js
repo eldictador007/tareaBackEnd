9e04e1f7e6fee4c919b1c17f60f48713146cf21b
eldictador007 <chantilli@protonmail.ch>
2023-03-01 19:27:00 -0300

    tarea1lista  para entrega RRr

--- tarea1.js
+++ tarea1.js
@@ -1,46 +1,46 @@
 class ProductManager {
     constructor() {
-      this.products = [];
-      this.lastId = 0;
+      this.products = []
+      this.lastId = 0
     }
   
     addProduct({title, description, price, thumbnail, code, stock}) {
       if (!title || !description || !price || !thumbnail || !code || !stock) {
-        console.error('All fields are mandatory');
+        console.error('Todos los campos son obligatorios')
         return;
       }
   
       const existingProduct = this.products.find(product => product.code === code);
       if (existingProduct) {
-        console.error('Product with the same code already exists');
+        console.error('Ya existe un producto con ese codigo')
         return;
       }
   
-      const id = ++this.lastId;
-      const newProduct = {id, title, description, price, thumbnail, code, stock};
-      this.products.push(newProduct);
-      console.log(`Product "${title}" added with id ${id}`);
+      const id = ++this.lastId
+      const newProduct = {id, title, description, price, thumbnail, code, stock}
+      this.products.push(newProduct)
+      console.log(`Se agrego el producto "${title}" con el siguiente id. ${id}`)
     }
   
     getProducts() {
-      return this.products;
+      return this.products
     }
   
     getProductById(id) {
-      const product = this.products.find(product => product.id === id);
+      const product = this.products.find(product => product.id === id)
       if (!product) {
-        console.error('Product not found');
+        console.error('No encontre lo que estas buscando')
       }
-      return product;
+      return product
     }
   }
-  
+
   
   // Ejemplo de uso
 
-  const productManager = new ProductManager();
+  const productManager = new ProductManager()
   
-  console.log(productManager.getProducts()); // []
+  console.log(productManager.getProducts())
   
   productManager.addProduct({
     title: 'producto prueba',
@@ -49,9 +49,10 @@ class ProductManager {
     thumbnail: 'Sin imagen',
     code: 'abc123',
     stock: 25,
-  }); 
+  })
   
-  console.log(productManager.getProducts()); 
+  console.log(productManager.getProducts());
+
   productManager.addProduct({
     title: 'otro producto',
     description: 'Este es otro producto',
@@ -59,8 +60,8 @@ class ProductManager {
     thumbnail: 'Otra imagen',
     code: 'abc123',
     stock: 10,
-  }); 
+  })
   
-  console.log(productManager.getProductById(2));
+  console.log(productManager.getProductById(2))
   
   console.log(productManager.getProductById(1));
