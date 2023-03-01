a5a306ab77758625b96281e66a11fc762af075d1
eldictador007 <chantilli@protonmail.ch>
2023-03-01 19:20:02 -0300

    tarea1 para entregar

---
+++ tarea1.js
@@ -0,0 +1,66 @@
+class ProductManager {
+    constructor() {
+      this.products = [];
+      this.lastId = 0;
+    }
+  
+    addProduct({title, description, price, thumbnail, code, stock}) {
+      if (!title || !description || !price || !thumbnail || !code || !stock) {
+        console.error('All fields are mandatory');
+        return;
+      }
+  
+      const existingProduct = this.products.find(product => product.code === code);
+      if (existingProduct) {
+        console.error('Product with the same code already exists');
+        return;
+      }
+  
+      const id = ++this.lastId;
+      const newProduct = {id, title, description, price, thumbnail, code, stock};
+      this.products.push(newProduct);
+      console.log(`Product "${title}" added with id ${id}`);
+    }
+  
+    getProducts() {
+      return this.products;
+    }
+  
+    getProductById(id) {
+      const product = this.products.find(product => product.id === id);
+      if (!product) {
+        console.error('Product not found');
+      }
+      return product;
+    }
+  }
+  
+  // Ejemplo de uso
+  const productManager = new ProductManager();
+  
+  console.log(productManager.getProducts()); // []
+  
+  productManager.addProduct({
+    title: 'producto prueba',
+    description: 'Este es un producto prueba',
+    price: 200,
+    thumbnail: 'Sin imagen',
+    code: 'abc123',
+    stock: 25,
+  }); // Product "producto prueba" added with id 1
+  
+  console.log(productManager.getProducts()); // [{id: 1, title: "producto prueba", description: "Este es un producto prueba", price: 200, thumbnail: "Sin imagen", code: "abc123", stock: 25}]
+  
+  productManager.addProduct({
+    title: 'otro producto',
+    description: 'Este es otro producto',
+    price: 100,
+    thumbnail: 'Otra imagen',
+    code: 'abc123', // Error: Product with the same code already exists
+    stock: 10,
+  }); 
+  
+  console.log(productManager.getProductById(2)); // Error: Product not found
+  
+  console.log(productManager.getProductById(1)); // {id: 1, title: "producto prueba", description: "Este es un producto prueba", price: 200, thumbnail: "Sin imagen", code: "abc123", stock: 25}
+  
